2dac03dd197bf1939501abe5e8e67a585df92d49
ostens <erinjayaustin@gmail.com>
2020-06-01 18:10:16 +0100

    Restructure tile state to use coordinates

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -3,68 +3,71 @@ import React from "react";
 import Board from "../Board/Board";
 import "./Game.scss";
 import Button from "../StyledComponents/Button/Button";
-import { randomTiles } from "./GameUtils";
+import { randomise, neighbors } from "./GameUtils";
 
 export class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tiles: randomTiles(),
-      numRows: 5
+      tiles: randomise(),
+      boardSize: 5
     }
   }
 
   handleClick(i) {
-    const tiles = this.state.tiles.slice();
-    const neighbors = this.handleBoundary(i);
-    neighbors.forEach(neighbor => tiles[neighbor] = !this.state.tiles[neighbor])
-    this.setState({
-      tiles: tiles.slice(0, 25),
+    const { boardSize } = this.state;
+    const { tiles } = this.state;
+    
+    const row = Math.floor(i / boardSize);
+    const column = i % boardSize;
+    
+    neighbors.forEach(coord => {
+      if (tiles[row + coord.x] !== undefined && tiles[row + coord.x][column + coord.y] !== undefined) {
+        tiles[row + coord.x][column + coord.y].lightOn = !this.state.tiles[row + coord.x][column + coord.y].lightOn
+      }
     });
-  }
-
-  handleBoundary(i) {
-    const { numRows } = this.state;
-    const leftEdge = [0, 5, 10, 15, 20];
-    const rightEdge = [4, 9, 14, 19];
-    const neighbors = [i, i + 1, i - 1, i + numRows, i - numRows];
-    if (leftEdge.includes(i)) {
-      return neighbors.filter(neighbor => neighbor !== i - 1);
-    } else if (rightEdge.includes(i)) {
-      return neighbors.filter(neighbor => neighbor !== i + 1);
-    } else {
-      return neighbors;
-    }
+    this.setState({
+      tiles,
+    });
   }
 
   handleReset() {
     this.setState({
-      tiles: randomTiles(),
+      tiles: randomise(),
     });
   }
 
+  calculateArray() {
+    const tiles = [];
+    Object.values(this.state.tiles).forEach(column => {
+      Object.values(column).forEach(tile => {
+        tiles.push(tile.lightOn)
+      })
+    })
+    return tiles;
+  }
+
   calculateWin() {
-    const currentTiles = this.state.tiles.slice();
+    const currentTiles = this.calculateArray();
     return currentTiles.includes(true) ? false : true;
   }
 
-render() {
-  const { tiles } = this.state;
-  const title = this.calculateWin() ? "You've won!" : "Lights out";
+  render() {
+    const title = this.calculateWin() ? "You've won!" : "Lights out";
 
-  return (
-    <div className="game">
-      <div className="title">{title}</div>
-      <div className="grid">
-        <Board
-          tiles={tiles}
-          onClick={(i) => this.handleClick(i)} />
+    return (
+      <div className="game">
+        <div className="title">{title}</div>
+        <div className="grid">
+          <Board
+            tiles={this.calculateArray()}
+            onClick={(i) => this.handleClick(i)} />
+        </div>
+        <Button
+          onClick={() => this.handleReset()} />
       </div>
-      <Button
-        onClick={() => this.handleReset()} />
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default Game;
--- src/Components/Game/GameUtils.js
+++ src/Components/Game/GameUtils.js
@@ -1,4 +1,19 @@
-export function randomTiles() {
-  const tiles = [...Array(25)].map(() => Math.random() <= 0.5);
+export function randomise() {
+  const tiles = {};
+  for (let x = 0; x < 5; x++) {
+    for (let y = 0; y < 5; y++) {
+      const lightOn = Math.random() <= 0.5;
+      if (tiles[x] === undefined) tiles[x] = {};
+      tiles[x][y] = { lightOn, coord: { x, y } };
+    }
+  };
   return tiles;
 }
+
+export const neighbors = [
+  { x: 0, y: 0 },
+  { x: 1, y: 0 },
+  { x: -1, y: 0 },
+  { x: 0, y: 1 },
+  { x: 0, y: -1 }
+];
